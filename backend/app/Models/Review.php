6658ab848d53ec62ffdb4a8be37e8714b1aaa25d
maruishi <ayaka0622.sakura@gmail.com>
2025-02-09 19:32:53 +0900

    test3

--- backend/app/Models/Review.php
+++ backend/app/Models/Review.php
@@ -9,6 +9,13 @@ class Review extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'user_id',
+        'movie_id',
+        'review_text',
+        'rating',
+    ];
+
     public function user() {
         return $this->belongsTo(User::class);
     }
