67383b174f0236792a4dfc7b48587b1fb344790d
maruishi <ayaka0622.sakura@gmail.com>
2025-02-11 02:44:55 +0900

    test6

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,6 +13,6 @@ Route::get('/reviews', [ReviewController::class, 'index']);
 Route::post('/reviews', [ReviewController::class, 'store']);
 Route::delete('/reviews/{review}', [ReviewController::class, 'destroy']);
 
+Route::get('/likes', [LikeController::class, 'index']);
 Route::post('/likes', [LikeController::class, 'toggleLike']);
-Route::get('/likes/status', [LikeController::class, 'checkLikeStatus']);
 Route::get('/likes/status', [LikeController::class, 'checkLikeStatus']);
